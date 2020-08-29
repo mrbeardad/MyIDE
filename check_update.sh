735062363f8e5b8f55694d9c60f944202f27278a
Heachen Bear <mrbeardad@qq.com>
2020-08-29 10:00:31 +0800

    improve both setting.json

--- check_update.sh
+++ check_update.sh
@@ -13,6 +13,7 @@ cp $option -vu ~/.cgdb/cgdbrc gdb
 cp $option -vu ~/.tmux.conf tmux/tmux.conf
 cp $option -vu ~/.zshrc zsh/zshrc
 cp $option -vu ~/WindowsHome/AppData/Local/Packages/Microsoft.WindowsTerminal_*/LocalState/settings.json win10/settings.json
+cp $option -vu ~/WindowsHome/AppData/Roaming/Code/User/settings.json vscode
 # cp $option -vu ~/WindowsHome/AppData/Roaming/alacritty/alacritty.yml alacritty/alacritty.yml
 cp -frvu ~/WindowsHome/AppData/Roaming/Rime/* rime 2> /dev/null
 
