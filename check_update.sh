f3e5f16ebe131a424daaf5cbfee9aa710afb8e78
Heache Bear <mrbeardad@qq.com>
2021-09-30 20:18:25 +0800

    update config of wt for ssh

--- check_update.sh
+++ check_update.sh
@@ -10,7 +10,6 @@ fi
 
 cp $option -vu ~/.gdbinit gdb/gdbinit
 cp $option -vu ~/.config/htop/htoprc htop/htoprc
-cp $option -vu ~/.ssh/ssh_config ssh/ssh_config
 cp $option -vu ~/.tmux.conf tmux/tmux.conf
 cp $option -vu ~/.zshrc zsh/zshrc
 cp $option -vu ~/.vscode-neovim/init.vim vscode/vscode-neovim/init.vim
