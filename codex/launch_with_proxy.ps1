aa81800b3af615c87ec80ba41787e88703c8bee2
Heache Bear <mrbeardad@qq.com>
2026-04-04 15:56:34 +0800

    fix(codex): change launch_with_proxy.ps1 to use $env

--- codex/launch_with_proxy.ps1
+++ codex/launch_with_proxy.ps1
@@ -1,20 +1,19 @@
 $ErrorActionPreference = 'Stop'
 
 try {
-    [Environment]::SetEnvironmentVariable('HTTP_PROXY', 'http://127.0.0.1:7890', 'User')
-    [Environment]::SetEnvironmentVariable('HTTPS_PROXY', 'http://127.0.0.1:7890', 'User')
-    [Environment]::SetEnvironmentVariable('ALL_PROXY', 'http://127.0.0.1:7890', 'User')
-    [Environment]::SetEnvironmentVariable('RUST_LOG', 'off', 'User')
+  $env:HTTP_PROXY='http://127.0.0.1:7890'
+  $env:HTTPS_PROXY='http://127.0.0.1:7890'
+  $env:ALL_PROXY='http://127.0.0.1:7890'
+  $env:RUST_LOG='off'
 
-    Start-Process 'shell:AppsFolder\OpenAI.Codex_2p2nqsd0c76g0!App'
-}
-catch {
-    $message = "launch_with_proxy.ps1 failed.`r`n`r`n$($_.Exception.Message)"
-    if ($_.ScriptStackTrace) {
-        $message += "`r`n`r`n$($_.ScriptStackTrace)"
-    }
+  & (Get-ChildItem "C:\Program Files\WindowsApps\OpenAI.Codex_*\app\Codex.exe" | Select-Object -Last 1 -ExpandProperty FullName)
+} catch {
+  $message = "launch_with_proxy.ps1 failed.`r`n`r`n$($_.Exception.Message)"
+  if ($_.ScriptStackTrace) {
+    $message += "`r`n`r`n$($_.ScriptStackTrace)"
+  }
 
-    $wsh = New-Object -ComObject WScript.Shell
-    $null = $wsh.Popup($message, 0, 'Codex Proxy Launcher', 16)
-    exit 1
+  $wsh = New-Object -ComObject WScript.Shell
+  $null = $wsh.Popup($message, 0, 'Codex Proxy Launcher', 16)
+  exit 1
 }
